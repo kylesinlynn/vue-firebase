d9efeb504c355671c4e787d6ffd467898da3fa55
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-04-03 15:11:09 +0630

    part 1

---
+++ app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+
+            title: 'The final empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
+})
+
+app.mount('#app')
